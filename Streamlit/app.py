1c527c4a1d247d0b3c513d1166307e441c272a47
Zachary Greenberg <zach.a.greenberg@gmail.com>
2021-05-12 20:48:19 -0400

    update streamlit code

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -28,7 +28,7 @@ with header:
     st.title('Wine Recommender System')
     st.text('By: Zachary Greenberg')
     
-    st.image('https://github.com/zachagreenberg/Wine_Recommendations/blob/main/Images/App.png')
+    st.image('https://github.com/zachagreenberg/Wine_Recommendations/blob/main/Images/App.jpg')
 
 with about:
     st.title('About')
